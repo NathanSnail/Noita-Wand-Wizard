7023374b907037f080276f2773f39099ba63d8f9
Nahtan <nathan.titirangi@gmail.com>
2023-06-28 16:05:27 +1200

    ro tat and for mat

--- main.py
+++ main.py
@@ -15,42 +15,45 @@ delta = np.zeros((sz[0], sz[1], 4), dtype="uint8")
 dims = [0, 0, sz[0], sz[1]]  # maxxy minxy
 thresh = 50.0
 for index, _ in np.ndenumerate(cliparr[:, :, 0]):
-	col = cliparr[index]
-	# print(col)
-	dist = np.linalg.norm(col.astype("float")-black.astype("float"))
-	# print(dist)
-	alpha = 0
-	if dist > thresh:
-		dims[0] = max(index[0], dims[0])
-		dims[1] = max(index[1], dims[1])
-		dims[2] = min(index[0], dims[2])
-		dims[3] = min(index[1], dims[3])
-		alpha = 255
-	delta[index[0], index[1], 0:3] = col
-	delta[index[0], index[1], 3] = alpha
+    col = cliparr[index]
+    # print(col)
+    dist = np.linalg.norm(col.astype("float")-black.astype("float"))
+    # print(dist)
+    alpha = 0
+    if dist > thresh:
+        dims[0] = max(index[0], dims[0])
+        dims[1] = max(index[1], dims[1])
+        dims[2] = min(index[0], dims[2])
+        dims[3] = min(index[1], dims[3])
+        alpha = 255
+    delta[index[0], index[1], 0:3] = col
+    delta[index[0], index[1], 3] = alpha
 
 # Image.fromarray(delta).show()
 # print(dims)
 
 cut = delta[dims[2]:dims[0], dims[3]:dims[1], :]
+if cut.shape[0] > cut.shape[1]:
+    cut = np.rot90(cut,3)
+
 stop = False
 im = Image.fromarray(cut)
 if not NO_SHOW:
-	im.show()
+    im.show()
 while not stop:
-	if not NO_SHOW:
-		print(im.size)
-	szi = input("Pixel size: ")
-	sz = []
-	if "," in szi:
-		sz = [float(x)/im.size[k] for k,x in enumerate(szi.split(","))]
-	else:
-		szi = [float(szi) for _ in range(2)]	
-	im2 = im.resize(
-		(round(im.size[0]/sz[0]), round(im.size[1]/sz[1])), Image.NEAREST)
-	if not NO_SHOW:
-		im2.show()
-	stop = input("good? Y/N: ").lower() == "y"
+    if not NO_SHOW:
+        print(im.size)
+    szi = input("Pixel size: ")
+    sz = []
+    if "," in szi:
+        sz = [float(x)/im.size[k] for k, x in enumerate(szi.split(","))]
+    else:
+        sz = [float(szi) for _ in range(2)]
+    im2 = im.resize(
+        (round(im.size[0]/sz[0]), round(im.size[1]/sz[1])), Image.NEAREST)
+    if not NO_SHOW:
+        im2.show()
+    stop = input("good? Y/N: ").lower() == "y"
 imarr = np.asarray(im2)
 
 
@@ -62,42 +65,45 @@ files = os.listdir(path+exts)
 minid = -1
 mind = 2**32
 for file in files:
-	if file == "custom":
-		continue
-	image = Image.open(os.path.join(path+exts, file)
-					   ).convert('RGBA').resize(im2.size)
-	arr = np.asarray(image).astype("float")
-	dist = np.linalg.norm(arr-imarr)
-	# print(dist)
-	if dist < mind:
-		mind = dist
-		minid = int(file[5:-4])
-	# print(arr.shape)
-	# print(image.mode)
+    if file == "custom":
+        continue
+    image = Image.open(os.path.join(path+exts, file)
+                       ).convert('RGBA').resize(im2.size)
+    arr = np.asarray(image).astype("float")
+    dist = np.linalg.norm(arr-imarr)
+    # print(dist)
+    if dist < mind:
+        mind = dist
+        minid = int(file[5:-4])
+    # print(arr.shape)
+    # print(image.mode)
 strid = str(minid)
 strid = "0"*(4-len(strid))+strid
 print(strid)
 if not NO_SHOW:
-	Image.open(path+exts+"\wand_"+strid+".png").show()
+    Image.open(path+exts+"\wand_"+strid+".png").show()
 
 with open(path+extw, "r") as file:
-	content = file.read()
+    content = file.read()
 
-data = eval("{"+"".join(
-	content[content.find(strid):]
-	.split("}")
-	[0]
-	.split("\n")
-	[5:])
-	.replace("=", "\":")
-	.replace("  ", "\"")
-	[:-1]
-	.replace(" \"", "\"")
-	+ "}"
+cmd = ("{"+"".join(
+    content[content.find(strid):]
+    .split("}")
+    [0]
+    .split("\n")
+    [5:])
+    .replace("=", "\":")
+    .replace("  ", "\"")
+    [:-1]
+    .replace(" \"", "\"")
+    + "}"
 )
+print(cmd)
+data = eval(cmd)
+
 data["fire_rate_wait"] = (data["fire_rate_wait"] + 1) * 7 - 5
 data["actions_per_round"] = data["actions_per_round"] + 1
-data["shuffle_deck_when_empty"] = data["shuffle_deck_when_empty"] == "1"
+data["shuffle_deck_when_empty"] = data["shuffle_deck_when_empty"] == 1
 data["deck_capacity"] = data["deck_capacity"] * 3 + 3
 data["spread_degrees"] = (data["spread_degrees"] + 1) * 5 - 5
 data["reload_time"] = (data["reload_time"] + 1) * 25 - 5
@@ -110,10 +116,10 @@ sz = data["deck_capacity"]
 sp = data["spread_degrees"]
 rc = data["reload_time"]
 print(f"""
-	Shuffle:		{"Yes" if sh else "No"}
-	Spells/Cast:	{sc:.0f}
-	Cast delay:		{cd / 60:.2f} s
-	Rechrg. Time:	{rc / 60:.2f} s
-	Capacity:		{sz:.0f}
-	Spread:			{sp:.1f} Deg
+Shuffle:        {"Yes" if sh else "No"}
+Spells/Cast:    {sc:.0f}
+Cast delay:     {cd / 60:.2f} s
+Rechrg. Time:   {rc / 60:.2f} s
+Capacity:       {sz:.0f}
+Spread:         {sp:.1f} Deg
 """)
